76770f9e24403181aac59ab2facdbba4e80d8881
Christian <christian.olsson@home.se>
2018-01-30 23:49:51 +0100

    Started documentation

---
+++ app/dao/bookFactory.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const defaultProperties = {
+  id: 0,
+  title: 'unknown title',
+  author: 'unknown author',
+  genre: 'unknown genre',
+  published: undefined,
+  price: undefined,
+  description: 'no description'
+}
+
+const bookPrototype = {}
+
+const createBook = function(bookProperties) {
+  const properties = Object.assign({}, defaultProperties, bookProperties)
+  return Object.assign(Object.create(bookPrototype), properties)
+}
+
+module.exports.createBook = createBook
